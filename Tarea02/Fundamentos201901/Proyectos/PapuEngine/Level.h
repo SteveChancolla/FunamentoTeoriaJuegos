101598cf70c1454a96322769cf34fc0df4501a82
steve willian <u201422771@upc.edu.pe>
2019-06-01 23:44:44 -0500

    Tarea2

---
+++ Tarea02/Fundamentos201901/Proyectos/PapuEngine/Level.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "SpriteBatch.h"
+using namespace std;
+
+const int TILE_WIDTH = 64;
+
+class Level
+{
+private:
+	vector<string> levelData;
+	int numHumans;
+	int numZombies;
+	void parseLevel();
+public:
+	glm::vec2 playerPosition;
+	glm::vec2 zombiePosition;
+	vector<glm::vec2> zombiesPosition;
+
+	glm::vec2 getPlayerPosition()const {
+		return playerPosition;
+	}
+	vector<glm::vec2> getZombiesPosition()const {
+		return zombiesPosition;
+	}
+	vector<string> getLevelData()const {
+		return levelData;
+	}
+
+	int getWidth() const {
+		return TILE_WIDTH;
+	}
+	int getHeight() const {
+		return TILE_WIDTH;
+	}
+
+	int getNumHumans()const {
+		return numHumans;
+	}
+	int getNumZombies()const {
+		return numZombies;
+	}
+	Level(const string& fileName);
+	void draw();
+	SpriteBatch spritebatch;
+	~Level();
+};
+
---
+++ Tarea02/Fundamentos201901/Proyectos/PapuEngine/Zombie.cpp
@@ -0,0 +1,36 @@
+#include "Zombie.h"
+#include <random>
+#include <ctime>
+#include <glm\gtx\rotate_vector.hpp>
+
+
+Zombie::Zombie()
+{
+}
+
+void Zombie::init(float _speed, glm::vec2 _position)
+{
+	speed = _speed;
+	position = _position;
+	color.r = 0;
+	color.g = 185;
+	color.b = 0;
+	color.a = 255;
+
+	static std::mt19937 randomEngine(time(nullptr));
+	static std::uniform_real_distribution<float>randDir(-1.0f, 1.0f);
+	direction = glm::vec2(randDir(randomEngine), randDir(randomEngine));
+	if (direction.length() == 0) {
+		direction = glm::vec2(1.0f, 1.0f);
+	}
+	direction = glm::vec2(direction);
+}
+
+void Zombie::update(const std::vector<std::string>& leveldata, 
+	std::vector<Human*>& humans, 
+	std::vector<Zombie*>& zombies){
+}
+
+Zombie::~Zombie()
+{
+}
